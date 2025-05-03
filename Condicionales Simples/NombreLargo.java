5cbe9d2964d90b43b9bb77f37b866dacec37c2ce
Nicolasruiz918 <nruizsastoque@gmail.com>
2025-05-02 19:10:37 -0500

    Update NombreLargo.java

--- Condicionales Simples/NombreLargo.java
+++ Condicionales Simples/NombreLargo.java
@@ -3,7 +3,7 @@ public class NombreLargo {
     public static void main(String[] args) {
         String nombre = "Alejandro"; 
         if (nombre.length() > 8) {
-            System.out.println("Nombre largo");
+            System.out.println("el nombre es mayor o igual a 8 caracteres");
         }
     }
 }
